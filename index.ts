bade25ab9a467a0689e2e9fdf8f85ff2845a6d6d
Renan Costa <renancostam@gmail.com>
2024-04-07 21:06:31 -0300

    Adjusting index exports

--- index.ts
+++ index.ts
@@ -22,6 +22,7 @@ export type {
 export type {
   BasePayload,
   BasePayloadMeta,
+  CompleteRegistrationPayload,
   CreateBusinessBillPayload,
   CreateBusinessUserPayload,
   CreateManagersPayload,
