97dd7072b07ec51ebdd69a37805d5a65fb57b73d
Renan Costa <renancostam@gmail.com>
2024-06-18 18:56:24 -0300

    Adding Payment descriptions

--- payment.ts
+++ payment.ts
@@ -8,9 +8,13 @@ export type PaymentMethod = "pix" | "billet" | "credits";
 export type Payment = {
   businessId: string;
   services: BusinessService[];
+  /** Sum of service values. */
   value: number;
+  /** Sum of discounts found <= to value. */
   discount: number;
+  /** Sum of fees charged for this payment. */
   fees: number;
+  /** Result value of the real payment transaction. */
   total: number;
   status: PaymentStatus;
   method?: PaymentMethod;
